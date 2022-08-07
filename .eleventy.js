942813adbc85bb8c646d23eedc098d6711fe0b20
Nolan Donahue <nolandonahue0@gmail.com>
2022-08-07 18:07:43 -0400

    added comments for TODO and BUGs

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,20 @@
 module.exports = (config) => {
+  // TODO mess around with passthrough once CSS figured out
   // config.addPassthroughCopy("/src/images/");
   // config.addPassthroughCopy("src/styles");
   // config.addPassthroughCopy("images");
   // config.addPassthroughCopy("styles");
 
+  config.addPassthroughCopy("root.css");
+
+  // TODO research how to have CSS update in npm start automatically
   config.addWatchTarget("/src/styles/");
   config.addWatchTarget("/src/images/");
   config.addWatchTarget("css");
   config.addWatchTarget("img");
 
   return {
+    // TODO the next 3 lines, look up them
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
@@ -20,6 +25,7 @@ module.exports = (config) => {
   };
 };
 
+// TODO finish figuring out CSS and then play with this
 // const CleanCSS = require("clean-css");
 // module.exports = function (eleventyConfig) {
 //   eleventyConfig.addFilter("cssmin", function (code) {
